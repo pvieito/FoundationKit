2ad76e2dfa55b0710105419ce4b9141f6e822c8f
Pedro José Pereira Vieito <pvieito@gmail.com>
2017-05-09 15:39:10 +0200

    Added appending(_) method for Arrays

--- FoundationKit/Array.swift
+++ FoundationKit/Array.swift
@@ -17,4 +17,12 @@ extension Array {
     public func appending<S>(contentsOf newElements: S) -> Array<Element> where S : Sequence, S.Iterator.Element == Element {
         return self + newElements
     }
+
+    /// Returns the array with the specified element appended.
+    ///
+    /// - Parameter newElement: Element appended to the returned array.
+    /// - Returns: Array with the element appended.
+    public func appending(_ newElement: Element) -> Array<Element> {
+        return self + [newElement]
+    }
 }
