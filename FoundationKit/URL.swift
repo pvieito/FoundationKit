96d17bc87f61f8b7475772014d95f0519ae1cc39
Pedro José Pereira Vieito <pvieito@gmail.com>
2018-04-16 21:57:40 +0200

    Revert "Added URL.cfURL property"
    This reverts commit bd1a0a045f15c90b07131bf979da472d9ec29120.

--- FoundationKit/URL.swift
+++ FoundationKit/URL.swift
@@ -11,14 +11,6 @@ import CoreFoundation
 
 extension URL {
     
-    public var cfURL: CFURL {
-        #if canImport(Darwin)
-        return self as CFURL
-        #else
-        return unsafeBitCast(self, to: CFURL.self)
-        #endif
-    }
-    
     /// Path Styles for a file URL.
     public enum PathStyle: Int, CustomStringConvertible {
         case posix = 0
@@ -61,11 +53,12 @@ extension URL {
             return nil
         }
         
-        let fileSystemPath = CFURLCopyFileSystemPath(self.cfURL, cfPathStyle)
-
         #if canImport(Darwin)
-        return fileSystemPath as String?
+        return CFURLCopyFileSystemPath(self as CFURL, cfPathStyle) as String?
         #else
+        let cfURL = unsafeBitCast(self, to: CFURL.self)
+        let fileSystemPath = CFURLCopyFileSystemPath(cfURL, cfPathStyle)
+        
         guard let cfPath = fileSystemPath else {
             return nil
         }
