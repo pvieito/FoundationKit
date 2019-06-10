59a3557fdd362ee3a3484824dc37efd09cd226d5
Pedro José Pereira Vieito <pvieito@gmail.com>
2019-06-10 21:23:44 +0200

    Better Info.plist descriptions

---
+++ FoundationKit/Dictionary.swift
@@ -0,0 +1,22 @@
+//
+//  Dictionary+PropertyList.swift
+//  BundleKit
+//
+//  Created by Pedro José Pereira Vieito on 10/06/2019.
+//  Copyright © 2019 Pedro José Pereira Vieito. All rights reserved.
+//
+
+import Foundation
+
+extension Dictionary {
+    var propertyList: String {
+        if let propertyListData = try? PropertyListSerialization.data(
+                fromPropertyList: self, format: .xml, options: 0),
+            let propertyListString = String(data: propertyListData, encoding: .utf8) {
+            return propertyListString
+        }
+        else {
+            return "<invalid>"
+        }
+    }
+}
