b1ff8940f60d5160d300fd89ff052b22e1320a39
Pedro José Pereira Vieito <pvieito@gmail.com>
2020-03-28 14:18:15 +0100

    [Foundation] Updated URL.platformPath(style:) API

--- FoundationKit/URL.swift
+++ FoundationKit/URL.swift
@@ -113,8 +113,8 @@ extension URL {
 }
 
 extension URL {
-    /// Path Styles for a file URL.
-    public enum PathStyle: Int, CustomStringConvertible {
+    /// Platform path styles for a file URL.
+    public enum PlatformPathStyle: Int, CaseIterable, CustomStringConvertible {
         case posix = 0
         case hfs
         case windows
@@ -139,13 +139,13 @@ extension URL {
         }
     }
     
-    /// Returns the path of a given URL.
+    /// Returns the platform path of a given URL.
     /// 
-    /// This function returns the URL's path as a file system path for a given path style.
+    /// This function returns the URL's path as a file system path for a given platform path style.
     ///
     /// - Parameter style: The operating system path style to be used to create the path.
     /// - Returns: The path in the specified style.
-    public func path(style: PathStyle) -> String? {
+    public func platformPath(style: PlatformPathStyle) -> String? {
         
         guard let cfPathStyle = style.cfPathStyle else {
             return nil
