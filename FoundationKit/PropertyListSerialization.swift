503744f04a7c77de1b31ac7ae1222b554614c23d
Pedro José Pereira Vieito <pvieito@gmail.com>
2024-03-19 18:22:15 +0100

    FoundationKit: Updated PropertyListSerialization extension

--- FoundationKit/PropertyListSerialization.swift
+++ FoundationKit/PropertyListSerialization.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 extension PropertyListSerialization {
-    public static func data(from propertyList: Any, format: PropertyListSerialization.PropertyListFormat = .xml, options: PropertyListSerialization.WriteOptions? = nil) throws -> Data {
-        return try self.data(fromPropertyList: propertyList, format: format, options: options ?? 0)
+    public static func data(from propertyList: Any, format: PropertyListSerialization.PropertyListFormat? = nil, options: PropertyListSerialization.WriteOptions? = nil) throws -> Data {
+        return try self.data(fromPropertyList: propertyList, format: format ?? .xml, options: options ?? 0)
     }
     
     public static func propertyList(from data: Data, options: PropertyListSerialization.ReadOptions = []) throws -> Any {
